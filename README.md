c8af51346363e4d7ae5399d8d50bc724731482f3
Raddy <r.angelov@live.co.uk>
2019-06-07 20:16:37 +0100

    final version

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ This code is part of a YouTube tutorial. The demo is aimed to convert a html ele
 
 [Link to YouTube Video](https://www.google.com)
 [Html2Canvas Link](https://html2canvas.hertzen.com/)
+
+
+There are no limits to what you can accomplish, except the limits you place on your own thinking. </br> - BRIAN TRACY -
--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-html2canvas(document.querySelector("#capture")).then(canvas => {
-    document.body.appendChild(canvas);
-});
+function generate() {
+    document.getElementById("render").innerHTML = "";
+    html2canvas(document.querySelector("#capture")).then(canvas => {
+        document.getElementById("render").appendChild(canvas);
+    });
+}
 
 function download() {
-    var download = document.getElementById("download");
-    var image = document.querySelector("canvas").toDataURL("image/png")
+    const download = document.getElementById("download");
+    let image = document.querySelector("canvas").toDataURL("image/png")
                         .replace("image/png", "image/octet-stream");
     download.setAttribute("href", image);
 }
 
+document.querySelector("textarea").addEventListener('keyup', function(){
+    const quoteText = document.getElementById("quote__text");
 
-
-
+    if(this.value != ""){
+        quoteText.innerHTML = this.value;
+        renderCanvas();
+    }
+    else {
+        quoteText.innerHTML = "Start typing </br>:)"
+    }
+    
+});
